e7700164814ca69d2d26f7be945a60cecc563c86
Jeppe <jeppe.birkholm.andersen@gmail.com>
2024-11-04 10:30:16 +0100

    funktionalitet tilføjet, Opgavelist og Opgaveinput

--- Task-Manager/src/SideBar.jsx
+++ Task-Manager/src/SideBar.jsx
@@ -1,19 +1,36 @@
 import './SideBar.css';
+import { useState } from 'react';
+import OpgaveInputPopUp from './OpgaveInput';
 
-/*Side bar komponentet*/
-function SideBar() {
+/* SideBar komponent til at vise en blaa sidebar med en knap til at tilføje opgaver */
+function SideBar({ onAddTask }) {
+        /* State til at håndtere visningen af popup*/
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+     /* Funktion der åbner og lukker popuppen*/
+    const togglePopup = () => {
+        setIsPopupOpen(!isPopupOpen);
+    };
+
     return (
         <div className="SideBar">
+                        {/* Overlay som vises når popup er blevet åbnet */}
+                        {isPopupOpen && <div className="overlay" onClick={togglePopup}></div>}
 
-            {/*Tilføj knap*/}
+
+                    {/* Knap til at åbne popup for at tilføje opgaver */}
             <div className='buttonContainer'>
-            <button className='plusKnap'><span></span><span className='vertikal'></span>
-            </button>
-            Tilføj opgave
+                <button className='plusButton' onClick={togglePopup}>
+                    <span></span>
+                    <span className='vertikal'></span>
+                </button>
+                Tilføj opgave
             </div>
-
+                        {/* Separator/divider mellem knappen og popup'en */}
             <div className='seperator'></div>
 
+                        {/* Popup komponenten der vises når isPopupOpen er sat til true */}
+            <OpgaveInputPopUp isOpen={isPopupOpen} onClose={togglePopup} onAddTask={onAddTask} />
         </div>
     );
 }
