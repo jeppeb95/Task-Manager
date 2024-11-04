b539f9f09410a05be43839afe603cefa0deeb1bf
Jeppe <jeppe.birkholm.andersen@gmail.com>
2024-11-04 10:47:22 +0100

    titel ændret

--- Task-Manager/src/OpgaveList.jsx
+++ Task-Manager/src/OpgaveList.jsx
@@ -7,7 +7,7 @@ function OpgaveList({ tasks, onClearTasks }) {
             <h3>Opgaveliste</h3>
         <div className='headlines-container'>
             <ul>
-                <li className='description-element'>Beskrivelse</li>
+                <li className='description-element'>Opgave</li>
                 <li>Dato</li>
                 <li>Type</li>
                 <li>Proritet</li>
